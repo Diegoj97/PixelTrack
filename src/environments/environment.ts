bc4c124ca7de51b874b16b89612439f22015b87d
Diego Jose Gutierrez Cano <d.gutierrez.jose@gmail.com>
2025-12-06 13:43:03 +0100

    creada coinexi0on a spotify

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  spotify: {
+    clientId: '07fd91fcefec4c1897df9593bf949475',
+    clientSecret: '774f962740754bddb4db4de857ffc280',
+    tokenUrl: 'https://accounts.spotify.com/api/token',
+    baseUrl: 'https://api.spotify.com/v1'
+  }
+};
