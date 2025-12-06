a5a21d2fe3a35519c6ccf4f7df9206a2d04c47d7
Diego Jose Gutierrez Cano <d.gutierrez.jose@gmail.com>
2025-12-06 13:18:58 +0100

    creado el navbar

---
+++ src/app/pixeltrack/components/album-image/album-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './album-image.component.html',
+  styleUrl: './album-image.component.css'
+})
+export class AlbumImageComponent {
+
+}
--- src/app/pixeltrack/pages/pixeltrack-page/pixeltrack-page.ts
+++ src/app/pixeltrack/pages/pixeltrack-page/pixeltrack-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BoardComponent } from '../../components/board/board.component';
 import { KeyBoardComponent } from '../../components/key-board/key-board.component';
+import { AlbumImageComponent } from '../../components/album-image/album-image.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-pixeltrack-page',
-  imports: [BoardComponent, KeyBoardComponent],
+  imports: [BoardComponent, KeyBoardComponent, AlbumImageComponent, NavbarComponent],
   templateUrl: './pixeltrack-page.html',
   styleUrl: './pixeltrack-page.css',
 })
